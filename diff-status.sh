1e6a97d4a7a14c3f7cef9f3858048e2ed5863b3a
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-03-20 07:52:46 +0100

    Fixes for #1 and #2 (based on new version of sparql integrate)

--- diff-status.sh
+++ diff-status.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Purpose: Prevent update of service statu time stamps if the status has not changed
+# Purpose: Prevent update of service status time stamps if the status has not changed
 
-# This script is in staging status and not yet active
 
+CURRENT="$1"
+NEXT="$2"
+
+[ -f "$CURRENT" ] || touch "$CURRENT"
 
 # Load the old status
 # Copy it to a named graph (we should add a short cut for this)
 # Laod the current status into the default graph
 # Replace new timestamps in the default graph with the old ones from the named graph if the status is still the same
-sparql-integrate --w=trig/pretty \
-  latest-status-old.ttl \
+java -cp si.jar sparqlintegrate --w=trig/pretty \
+  "$CURRENT" \
   'DELETE { ?s ?p ?o } INSERT { GRAPH eg:old { ?s ?p ?o } } WHERE { ?s ?p ?o }' \
-  latest-status.ttl \
-  'PREFIX : <https://schema.org/> DELETE { ?s :dateModified ?newTime } INSERT { ?s :dateModified ?oldTime } WHERE { ?s :serverStatus ?newStatus ; :dateModified ?newTime GRAPH ?g { ?s :serverStatus ?oldStatus ; :dateModified ?oldTime } FILTER(?oldStatus = ?newStatus) }' \
-  spo.sparql > new-status.ttl
+  "$NEXT" \
+  'PREFIX schema: <https://schema.org/> DELETE { ?s schema:dateModified ?newTime } INSERT { ?s schema:dateModified ?oldTime } WHERE { ?s schema:serverStatus ?newStatus ; schema:dateModified ?newTime GRAPH ?g { ?s schema:serverStatus ?oldStatus ; schema:dateModified ?oldTime } FILTER(?oldStatus = ?newStatus) }' \
+  'CONSTRUCT WHERE { ?s ?p ?o } ORDER BY ?s ?p ?o'
+
+
--- update-status.sh
+++ update-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download SPARQL Integrate (if not already available)
-[ -f si.jar ] || wget -O si.jar https://github.com/SmartDataAnalytics/SparqlIntegrate/releases/download/sparql-integrate-bundle-1.0.2-SNAPSHOT/sparql-integrate-bundle-1.0.2-SNAPSHOT-jar-with-dependencies.jar
+[ -f si.jar ] || wget -O si.jar https://github.com/SmartDataAnalytics/SparqlIntegrate/releases/download/sparql-integrate-bundle-1.0.3-SNAPSHOT/sparql-integrate-bundle-1.0.3-SNAPSHOT-jar-with-dependencies.jar
 
 # TODO Update lod cloud status once the PR is accepted
 # wget -O LODCloud_SPARQL_Endpoints.ttl https://raw.githubusercontent.com/OpenLinkSoftware/general-turtle-doc-collection/master/LODCloud_SPARQL_Endpoints.ttl
@@ -11,5 +11,10 @@
 # and then merge the results
 java -cp si.jar sparqlintegrate LODCloud_SPARQL_Endpoints.ttl 'CONSTRUCT { GRAPH ?o { ?s ?p ?o } } { ?s ?p ?x FILTER(?p = sd:endpoint) BIND(IRI(REPLACE(STR(?x), "\\s+", "")) AS ?o) }' | \
 java -cp si.jar ngs map -t '5000,5000' --sparql status-check.sparql | \
-java -cp si.jar sparqlintegrate --w=trig/pretty --u - 'PREFIX schema: <https://schema.org/> CONSTRUCT WHERE { ?s ?p ?o }' > latest-status.ttl
+java -cp si.jar sparqlintegrate --w=trig/pretty --u - 'PREFIX schema: <https://schema.org/> CONSTRUCT WHERE { ?s ?p ?o } ORDER BY ?s ?p ?o' > next-status.ttl
+
+./diff-status.sh latest-status.ttl next-status.ttl > effective-status.ttl
+
+mv effective-status.ttl latest-status.ttl
+
 
